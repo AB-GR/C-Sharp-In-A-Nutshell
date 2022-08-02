a77b1bd9f85f5897d41a2843351be4da346826fd
Abhilash <abhilashgraja@gmail.com>
2022-08-02 17:04:51 +0530

    arrays parameter types

--- CSharpExercises/TypeBasics.cs
+++ CSharpExercises/TypeBasics.cs
@@ -1,4 +1,6 @@
-﻿namespace CSharpExercises
+﻿using System.Text;
+
+namespace CSharpExercises
 {
 	internal class TypeBasics
 	{
@@ -39,8 +41,8 @@
 			Console.WriteLine(p2Ref.X);
 
 			//Predefined types in C# alias .NET types in the System namespace
-			int i = 32;
-			System.Int32 j = 32;
+			int i1 = 32;
+			System.Int32 j1 = 32;
 
 			// Insert _ to make more readable
 			int million = 1_000_000;
@@ -57,15 +59,15 @@
 
 			checked
 			{
-				int a = int.MinValue;
+				int a10 = int.MinValue;
 				//a--;
-				Console.WriteLine(a == int.MaxValue); // True
+				Console.WriteLine(a10 == int.MaxValue); // True
 			}
 
 			unchecked {
-				int a = int.MinValue;
-				a--;
-				Console.WriteLine(a == int.MaxValue); // True
+				int a12 = int.MinValue;
+				a12--;
+				Console.WriteLine(a12 == int.MaxValue); // True
 			}
 
 			int y = unchecked(int.MaxValue + 1);
@@ -105,6 +107,133 @@ Second Line";
 
 			const string greeting = "Hello";
 			const string message = $"{greeting}, world";
+
+
+			char[] vowels = new char[] { 'a', 'e', 'i', 'o', 'u' };
+
+			// Or
+			char[] vowels1 = { 'a', 'e', 'i', 'o', 'u' };
+			Index last = ^1;
+			Index first = 0;
+			var firstElement = vowels1[first];
+			var lastElement = vowels1[last];
+			var secondLastElement = vowels1[^2];
+
+			char[] firstTwo = vowels[..2];    // 'a', 'e'
+			char[] lastThree = vowels[2..];    // 'i', 'o', 'u'
+			char[] middleOne = vowels[2..3];   // 'i'
+			char[] lastTwo = vowels[^2..];     // 'o', 'u'
+			Range firstTwoRange = 0..2;
+			char[] firstTwoWithRange = vowels[firstTwoRange];   // 'a', 'e'
+
+			//var error = vowels1[^0]; //^0 represents the length of the array, so results in IndexOutOfRange Exception
+
+			// An array itself is always a reference type object, regardless of the element type. For instance, the following is legal:
+			int[] a = null;
+
+			// Rectangular array
+			int[,] matrix = new int[3, 3];
+			for (int i = 0; i < matrix.GetLength(0); i++)
+				for (int j = 0; j < matrix.GetLength(1); j++)
+					matrix[i, j] = i * 3 + j;
+
+			int[,] matrix1 = new int[,]
+							{
+							  {0,1,2},
+							  {3,4,5},
+							  {6,7,8}
+							};
+
+			// jagged
+			int[][] jaggedMatrix = new int[3][];
+			for (int i = 0; i < jaggedMatrix.Length; i++)
+			{
+				jaggedMatrix[i] = new int[3];                    // Create inner array
+				for (int j = 0; j < jaggedMatrix[i].Length; j++)
+					jaggedMatrix[i][j] = i * 3 + j;
+			}
+
+			int[][] jaggedMatrix1 = new int[][]
+							{
+							  new int[] {0,1,2},
+							  new int[] {3,4,5},
+							  new int[] {6,7,8,9}
+							};
+
+			// Implicit typing can be taken one stage further with arrays: you can omit the type qualifier after the new keyword and have the compiler infer the array type:
+			var vowels2 = new[] { 'a', 'e', 'i', 'o', 'u' };   // Compiler infers char[]
+			//Split("Abhilash G Raja", out string x1, out _);// _ is a discard
+			//Console.WriteLine(x1);
+
+			//For backward compatibility, this language feature will not take effect if a real underscore variable is in scope:
+			string _;
+			Split("Stevie Ray Vaughan", out string x2, out _);
+			Console.WriteLine(_);     // Vaughan
+
+			//An in parameter is similar to a ref parameter except that the argument’s value cannot be modified by the method (doing so generates a compile-time error)
+			int[] ints = { 1, 2, 3, 4 };
+			ref int ref2 = ref ints[2];
+			Console.WriteLine(ints[2]);
+			ref2++;
+			Console.WriteLine(ints[2]);
+
+			ref var ref3 = ref GetRandomStringValue();
+			Console.WriteLine(randomStringValue);
+			ref3 = "Not so random";
+			Console.WriteLine(randomStringValue);
+
+			var ref4 = GetRandomStringValueReadOnly();
+			ref4 = "dfdf";
+			Console.WriteLine(randomStringValue);
+			MyMethod(new("Hello"));//Target typed new Expressions
+
+			string foo = null;
+			char? c = foo?[1];  // c is null
+			GuessTheType(true);
+			GuessTheType("Hello");
+			Console.WriteLine(GuessTheCard(13));
+
+		}
+		static void MyMethod(StringBuilder str)
+		{
+
+		}
+
+		static string GuessTheCard(int card) => card switch { 
+			13 => "King",
+			12 => "Queen",
+			11 => "Jack",
+			_ => "Pip Card"
+		};
+
+		static void GuessTheType(object x)
+		{
+			switch(x)
+			{
+				case bool b when true:
+					Console.WriteLine($"Is a bool with value {b}");
+					break;
+				default:
+					Console.WriteLine("Unknown");
+					break;
+			}
+		}
+
+		static ref string RandomStringProp => ref randomStringValue;
+
+		static ref string GetRandomStringValue()
+			=> ref randomStringValue;
+
+		static ref readonly string GetRandomStringValueReadOnly()
+			=> ref randomStringValue;
+
+		static string randomStringValue = "Random Value";
+
+		static void Split(string name, out string firstNames, out string lastName)
+		{
+			int i = name.LastIndexOf(' ');
+			firstNames = name.Substring(0, i);
+			lastName = name.Substring(i + 1);
 		}
 	}
 
